76fa9fbd46debaaf0f54e0471f177cc86d45b758
wanglawrence9995 <wanglawrence9995@gmail.com>
2019-02-19 07:17:40 -0600

    first init

---
+++ src/extension.ts
@@ -0,0 +1,55 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { IORunManager } from './ioRunManager';
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	//	console.log('Congratulations, your extension "cpcomplete" is now active!');
+	let config = vscode.workspace.getConfiguration('io-run');
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	let ioRunManager = new IORunManager(config);
+
+    vscode.workspace.onDidChangeConfiguration(() => {
+        config = vscode.workspace.getConfiguration('io-run');
+        ioRunManager.updateConfig(config);
+   //     analytics.updateConfig(config);
+    });
+
+
+    let run = vscode.commands.registerCommand('io-run.run', () => {
+        ioRunManager.run();
+    });
+    let run1input = vscode.commands.registerCommand('io-run.run-1-input', () => {
+        ioRunManager.run(false);
+    });
+
+    let stop = vscode.commands.registerCommand('io-run.stop', () => {
+        ioRunManager.stop();
+    });
+
+    let addInputOutput = vscode.commands.registerCommand('io-run.add-input-output', () => {
+        ioRunManager.addInputOutput();
+    });
+
+    context.subscriptions.push(run);
+    context.subscriptions.push(run1input);
+    context.subscriptions.push(stop);
+    context.subscriptions.push(addInputOutput);
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
---
+++ src/tools.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+export function getFileNoExtension(filePath: string): string {
+    let index = filePath.lastIndexOf(".");
+    if (index !== -1) {
+        return filePath.substr(0, index);
+    } else {
+        return filePath;
+    }
+}
+
+export function quoteFileName(filePath: string): string {
+    if (filePath.match(/\s/)) {
+        filePath = '"' + filePath.replace(/"/g, '\"') + '"'
+    }
+    return filePath;
+}
+
+
+export function replaceVar(originalStr: string, varName: string, value: string): string {
+    let regx = new RegExp("\\$\\{" + varName + "([\\W][^ }]+)?\\}", "g");
+
+    if (value.match(/\s/)) {
+        value = value.replace(/"/g, '\\"');
+        return originalStr.replace(regx, '"' + value + '$1"');
+    }
+
+    return originalStr.replace(regx, value + '$1');
+}
+
+
+export function unwrap(proxy): object {
+    if (typeof proxy !== 'object') {
+        return proxy;
+    }
+    let obj = {};
+    Object.keys(proxy).forEach(function (key) {
+        let s = proxy[key];
+        let u = unwrap(s);
+        obj[key] = u;
+    });
+    return obj;
+}
